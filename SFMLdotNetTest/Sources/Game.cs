4437a483f69a37ad7f8415eb9fba702fa940a41f
TRheed <rhksghd8303@naver.com>
2019-06-25 17:01:32 +0900

    광자 생성 간격 0.01 픽셀 -> 0.002 픽셀로 변경

--- SFMLdotNetTest/Sources/Game.cs
+++ SFMLdotNetTest/Sources/Game.cs
@@ -30,8 +30,8 @@ namespace BlackHoleVisualization
             particles = new List<Photon>();
 
             var start = WinSizeY * 0.078f;
-            var end = WinSizeY * 0.0843f;//WinSizeY / 2f - blackHole.Radious * 2.6f;
-            for (var y = start; y < end; y += 0.01f)
+            var end = WinSizeY * 0.0843f;
+            for (var y = start; y < end; y += 0.002f)
                 particles.Add(new Photon(WinSizeX, y));
 
         }
