c93285ee5536cc52a13e63bda104ed93463b9695
TRheed <rhksghd8303@naver.com>
2019-06-28 17:18:28 +0900

    파라미터 세팅 변경

--- Project/Scripts/Game/Photon.cs
+++ Project/Scripts/Game/Photon.cs
@@ -96,7 +96,7 @@ namespace BlackHoleVisualization
         {
             if (this.IsDisappeared) return;
 
-            DisplayPhoton();
+            //DisplayPhoton();
             DisplayHistory();
         }
 
